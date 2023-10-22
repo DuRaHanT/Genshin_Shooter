a4c5d2869d4f13d27791472683a4c95346ce0372
yukae <kms980518@gmail.com>
2023-10-22 18:15:35 +0900

    .

--- Assets/Script/Yukae/Bullet.cs
+++ Assets/Script/Yukae/Bullet.cs
@@ -4,4 +4,7 @@ using UnityEngine;
 
 public class Bullet : BulletBase
 {
+    public int BulletDamege => bulletSetting.bulletDamage;
+    public int currentBullet => bulletSetting.currentBullet;
+    public int possessionBullet => bulletSetting.possessionBullet;
 }
--- Assets/Script/Yukae/Grenade.cs
+++ Assets/Script/Yukae/Grenade.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Grenade : GrenadeBase
 {
+    public int GrenadeDamege => grenadeSetting.grenadeDamage;
+    public int currentGrenade => grenadeSetting.currentGrenade;
+    public int possessionGrenade => grenadeSetting.possessionGrenade;
+
     public override void StartWeaponAction()
     {
     }
